8108d27bb3ec592536a659cffea3fbb97cab0a66
Kim Jung Yeon <zoemfhs123@gmail.com>
2019-10-30 23:40:27 +0900

    GameMode & PlayerController & Pawn
    각각의 메서드에 로그를 남겨 플레이어가 게임에 입장하고 세팅이 완료되기 까지의 과정을 기록하였다.
    ArenaBattle: Warning: AABPlayerController::PostInitializeComponents(10)
    ArenaBattle: Warning: AABGameMode::PostLogin(16) PostLogin Begin
    ArenaBattle: Warning: AABPawn::PostInitializeComponents(32)
    ArenaBattle: Warning: AABPawn::PossessedBy(39)
    ArenaBattle: Warning: AABPlayerController::OnPossess(17)
    ArenaBattle: Warning: AABGameMode::PostLogin(18) PostLogin End

---
+++ ArenaBattle/ABPlayerController.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPlayerController.h"
+
+void AABPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	ABLOG_S(Warning);
+}
+
+void AABPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	ABLOG_S(Warning);
+	ABLOG(Warning, TEXT(InPawn->GetName()))
+}
